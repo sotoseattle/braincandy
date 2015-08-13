eda215b9febe1c93a83ad4a7f973b20617d521fa
Javier Soto <sotoseattle@gmail.com>
2015-08-12 19:41:37 -0700

    thanks Ryan, didnt know transpose existed. Your clevar sol kicks ass

--- lazy-professor/lib/grader_hashy.rb
+++ lazy-professor/lib/grader_hashy.rb
@@ -2,17 +2,10 @@ module GraderHashy
   def self.cheat filename
     data_matrix = File.open(filename).read.split("\n").map(&:chars)
 
-    transpose(data_matrix).map do |a|
+    data_matrix.transpose.map do |a|
       freq = a.inject(Hash.new(0)) { |h, v| h[v] += 1; h }
       a.max_by { |v| freq[v] }
     end.join
-  end
-
-  def self.transpose matrix
-    neo = []
-    matrix.first.size.times { neo << [] }
-    matrix.each { |row| row.each_with_index { |x, i| neo[i] << x } }
-    neo
   end
 end
 
