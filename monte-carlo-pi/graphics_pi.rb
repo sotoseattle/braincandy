f204ace3b5cbd6e389874e46ffc0eda523e02321
Javier Soto <sotoseattle@gmail.com>
2015-08-28 10:32:48 -0700

    refactor, simplify, add the value of pi to title window

--- monte-carlo-pi/graphics_pi.rb
+++ monte-carlo-pi/graphics_pi.rb
@@ -1,34 +1,29 @@
 require 'graphics'
+require 'pry'
 
-class Shot < Graphics::Body
-  def d p
-    (self.x - p.x)**2 + (self.y - p.y)**2
+class Dot < Graphics::Body
+  def distance_to_origin
+    self.x**2 + self.y**2
   end
 end
 
 class Piripi < Graphics::Simulation
-  attr_reader :origin
-  MULTIPLIER = 100
+  MULTIPLIER = 500
 
   def initialize
-    @r = 801
-    super @r, @r
-
-    @origin = Shot.new(self)
-    @origin.position = V::ZERO
+    @radius = 800
     @count_inside = 0
+    super @radius, @radius
   end
 
   def draw n
-    # clear :black
-    circle 0, 0, @r, :white
-    text pi(n+1), 0, 700, :white
+    SDL::WM.set_caption pi_text(n+1), ''
   end
 
   def update n
     MULTIPLIER.times do
-      shot = Shot.new(self)
-      color = if @origin.d(shot) <= @r**2
+      shot = Dot.new(self)
+      color = if shot.distance_to_origin <= @radius**2
                 @count_inside += 1
                 :green
               else
@@ -38,8 +33,8 @@ class Piripi < Graphics::Simulation
     end
   end
 
-  def pi n
-    "%1.5f" % [4.0 * @count_inside / n / MULTIPLIER]
+  def pi_text n
+    "Shotgun Pi: " + "%1.5f" % [4.0 * @count_inside / n / MULTIPLIER]
   end
 end
 
